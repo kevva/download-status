cedcd93ba1d3c2d0c2e1b04abf95713fd64c3e7b
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-15 14:29:28 +0200

    Only show progress bar if content-length could be parsed

--- index.js
+++ index.js
@@ -15,26 +15,28 @@ module.exports = function (opts) {
 	return function (res, url, cb) {
 		opts = opts || { info: 'cyan' };
 
-		var msg = chalk[opts.info]('  downloading') + ' : ' + url;
-		var info = chalk[opts.info]('     progress') + ' : [:bar] :percent :etas';
-		var len = parseInt(res.headers['content-length'], 10);
+		if (res.headers['content-length']) {
+			var msg = chalk[opts.info]('  downloading') + ' : ' + url;
+			var info = chalk[opts.info]('     progress') + ' : [:bar] :percent :etas';
+			var len = parseInt(res.headers['content-length'], 10);
 
-		var bar = new ProgressBar(info, assign({
-			complete: '=',
-			incomplete: ' ',
-			width: 20,
-			total: len
-		}, opts));
+			var bar = new ProgressBar(info, assign({
+				complete: '=',
+				incomplete: ' ',
+				width: 20,
+				total: len
+			}, opts));
 
-		console.log(msg);
+			console.log(msg);
 
-		res.on('data', function (data) {
-			bar.tick(data.length);
-		});
+			res.on('data', function (data) {
+				bar.tick(data.length);
+			});
 
-		res.on('end', function () {
-			console.log('\n');
-			cb();
-		});
+			res.on('end', function () {
+				console.log('\n');
+				cb();
+			});
+		}
 	};
 };
