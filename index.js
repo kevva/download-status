dca9a413d00d4d858dd02b09e5bc87508835324f
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-15 14:33:22 +0200

    Align using `lpad-align`

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var assign = require('object-assign');
 var chalk = require('chalk');
+var lpad = require('lpad-align');
 var ProgressBar = require('progress');
 
 /**
@@ -13,22 +14,28 @@ var ProgressBar = require('progress');
 
 module.exports = function (opts) {
 	return function (res, url, cb) {
-		opts = opts || { info: 'cyan' };
+		opts = opts || {};
 		opts.stream = opts.stream || process.stderr;
+		opts.indent = opts.indent || 2;
 
 		if (res.headers['content-length']) {
-			var msg = chalk[opts.info]('  downloading') + ' : ' + url;
-			var info = chalk[opts.info]('     progress') + ' : [:bar] :percent :etas';
-			var len = parseInt(res.headers['content-length'], 10);
+			var words = [
+				'fetch',
+				'progress'
+			];
 
-			var bar = new ProgressBar(info, assign({
+			var fetch = chalk.cyan(lpad('fetch', words, opts.indent));
+			var progress = chalk.cyan(lpad('progress', words, opts.indent));
+			var str = progress + ' : [:bar] :percent :etas';
+
+			var bar = new ProgressBar(str, assign({
 				complete: '=',
 				incomplete: ' ',
 				width: 20,
-				total: len
+				total: parseInt(res.headers['content-length'], 10)
 			}, opts));
 
-			opts.stream.write(msg);
+			opts.stream.write(fetch + ' : ' + url + '\n');
 
 			res.on('data', function (data) {
 				bar.tick(data.length);
